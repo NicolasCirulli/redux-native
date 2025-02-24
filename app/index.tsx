4549485a965e90a181df67c982c05d407d28f539
nicolascirulli <nicolascirulli2@gmail.com>
2025-02-24 09:01:41 -0300

    workshop 21-02

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,80 @@
-import { Redirect } from "expo-router";
+import { Redirect, router } from "expo-router";
 import "../global.css";
 import storage from "../src/utils/storage";
 import useUser from "../src/store/hooks/useUserActions";
 import AuthServices from "../src/services/user.services";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { Text, View, PermissionsAndroid } from "react-native";
+import {
+  AuthorizationStatus,
+  getMessaging,
+  getToken,
+  onMessage,
+  onNotificationOpenedApp,
+  requestPermission,
+  setBackgroundMessageHandler,
+} from "@react-native-firebase/messaging";
+
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
+const messaging = getMessaging();
+
+async function requestUserPermission() {
+  const authStatus = await requestPermission(messaging);
+  const enabled =
+    authStatus === AuthorizationStatus.AUTHORIZED ||
+    authStatus === AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    return enabled;
+  }
+  return false;
+}
 
 export default function index() {
+  const [permission, setPermission] = useState<
+    typeof AuthorizationStatus | boolean
+  >(false);
   const [appIsReady, setAppIsReady] = useState(false);
   const { login } = useUser();
+
+  useEffect(() => {
+    requestUserPermission().then(setPermission);
+  }, []);
+
+  useEffect(() => {
+    if (permission) {
+      getToken(messaging).then((token) => {
+        //....
+        console.log(token);
+      });
+
+      onMessage(messaging, async (payload) => {
+        console.log("onMessage: ", payload.notification);
+        console.log("onMessage: ", payload.data);
+        //....
+      });
+
+      setBackgroundMessageHandler(messaging, async (payload) => {
+        //...
+        console.log("background: ", payload.notification);
+      });
+
+      onNotificationOpenedApp(messaging, async (payload) => {
+        //...
+        console.log("openApp: ", payload.notification);
+        if (payload?.data?.route) {
+          router.push("/" + payload?.data?.route);
+        }
+      });
+    }
+  }, [permission]);
 
   useEffect(() => {
     storage.getItem("token").then((token) => {
       if (token) {
         AuthServices.loginWithToken(token)
           .then((data) => {
-            console.log(data);
             if (data) {
               login(data);
             }
---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeQ08YtpMJQqDmLYXeIWR6iU58l2QXMN8",
+  authDomain: "notifications-console.firebaseapp.com",
+  projectId: "notifications-console",
+  storageBucket: "notifications-console.firebasestorage.app",
+  messagingSenderId: "961094733271",
+  appId: "1:961094733271:web:5319344ccf3cd93cdb4f92",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
